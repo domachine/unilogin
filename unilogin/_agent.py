e5a7daa1b4ae4a4660a49b1902eb5d230a720968
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2010-11-19 08:26:13 +0100

    Added agent-regex-compile feature.

--- unilogin/_agent.py
+++ unilogin/_agent.py
@@ -17,6 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with unilogin.  If not, see <http://www.gnu.org/licenses/>.
 
+from urllib.request import urlopen
+
+
 __all__ = ["Agent"]
 
 class LoginManager(object):
@@ -25,12 +28,13 @@ class LoginManager(object):
     An object of this class is usually created through the Agent.
     """
 
-    def __init__(self, url, data):
+    def __init__(self, parent, url, data):
         """
         Initializes the Manager with the form data and the
         url to call.
         """
 
+        self._parent = parent
         self._url = url
         self._data = data
 
@@ -55,6 +59,7 @@ class Agent(object):
         self._form_regex = None
         self._data_regex = None
         self._end_form_regex = None
+        self._end_form_regex = None
 
     def login_manager(self, register_url="http://uni-ulm.de"):
         """
@@ -68,3 +73,40 @@ class Agent(object):
 
         # TODO: Implement it :-)
         pass
+
+    @property
+    def form_regex(self):
+        if self._form_regex is None:
+            self._compile_regex("_form_regex",
+                                r'.*form *method="post" *action="([^"]+)".*',
+                                re.IGNORECASE)
+
+        return self._form_regex
+
+    @property
+    def data_regex(self):
+        if self._data_regex is None:
+            self._compile_regex("_data_regex",
+                                r'.*type="hidden" *name="([^"]+)" *value="([^"]*)".*',
+                                re.IGNORECASE)
+
+        return self._data_regex
+
+    @property
+    def end_form_regex(self):
+        if self._end_form_regex is None:
+            self._compile_regex("_end_form_regex",
+                                r"[ \t]*<\/ *form *>[ \t]*",
+                                re.IGNORECASE)
+
+        return self._end_form_regex
+
+    @property
+    def logout_regex(self):
+        if self._logout_regex is None:
+            self._compile_regex("_logout_regex",
+                                r".*<h1>Logout OK<\/h1>.*",
+                                re.IGNORECASE)
+
+    def _compile_regex(name, pattern):
+        self.__dict__[name] = re.compile(pattern)
